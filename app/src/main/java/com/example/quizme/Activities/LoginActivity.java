b5301171cb15b58a14adc2c3f39ae35de981b926
hsprabhakar <saurab.harish@gmail.com>
2021-12-16 19:29:04 -0700

    Created basic login page

--- app/src/main/java/com/example/quizme/Activities/LoginActivity.java
+++ app/src/main/java/com/example/quizme/Activities/LoginActivity.java
@@ -1,4 +1,37 @@
 package com.example.quizme.Activities;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quizme.R;
+import com.google.android.material.button.MaterialButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        TextView username = findViewById(R.id.username_edittext);
+        TextView password = findViewById(R.id.password_edittext);
+
+        MaterialButton loginButton = findViewById(R.id.login_button);
+
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // check if username matches the db. if so, check the isPassword() function as to whether right password
+                // idk man just copy heebit
+            }
+        });
+
+    }
 }
